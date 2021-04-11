377260156056707b7918be09041451d8c123c052
willwalsh96 <williamtecumsehwalsh@gmail.com>
2021-04-10 23:20:11 -0500

    install anal geg add tag

--- gatsby-config.js
+++ gatsby-config.js
@@ -73,7 +73,20 @@ module.exports = {
       },
     ],
   },
+
   plugins: [
+
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          'G-03L081TSWJ', // Google Analytics / GA
+          "AW-CONVERSION_ID", // Google Ads / Adwords / AW
+          "DC-FLOODIGHT_ID", // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
+        ],
+      },
+    },
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
