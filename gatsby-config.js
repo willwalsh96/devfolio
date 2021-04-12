ef77ecd154c581fb77d32002f81fa97923ae16be
willwalsh96 <williamtecumsehwalsh@gmail.com>
2021-04-11 22:23:21 -0500

    comment out lies fixe

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,62 +19,62 @@ module.exports = {
     I gained access to highspeed internet at the tender age of 9, and since then I've been learning incessantly.
     Now that I'm 25 I recognize how important programming skills can be..... `,
     // Optional: List your projects, they must have `name` and `description`. `link` is optional.
-  //   projects: [
-  //     {
-  //       name: 'Devfolio',
-  //       description:
-  //         'A zero-config and blazing fast personal site + blog built with GatsbyJs and TailwindCSS',
-  //       link: 'https://github.com/RyanFitzgerald/devfolio',
-  //     },
-  //     {
-  //       name: 'ChromeExtensionKit',
-  //       description:
-  //         'Kit to jump-start your Chrome extension projects with a variety of battle-tested starter templates',
-  //       link: 'https://chromeextensionkit.com/?ref=devfolio',
-  //     },
-  //     {
-  //       name: 'Another Cool Project',
-  //       description:
-  //         'Lorem ipsum dolor sit amet consectetur adipisicing elit ducimus perferendis',
-  //       link: 'https://github.com/RyanFitzgerald/devfolio',
-  //     },
-  //   ],
-  //   // Optional: List your experience, they must have `name` and `description`. `link` is optional.
-  //   experience: [
-  //     {
-  //       name: 'Acme Corp',
-  //       description: 'Full-Stack Developer, February 2020 - Present',
-  //       link: 'https://github.com/RyanFitzgerald/devfolio',
-  //     },
-  //     {
-  //       name: 'Globex Corp',
-  //       description: 'Full-Stack Developer, December 2017 - February 2020',
-  //       link: 'https://github.com/RyanFitzgerald/devfolio',
-  //     },
-  //     {
-  //       name: 'Hooli',
-  //       description: 'Full-Stack Developer, May 2015 - December 2017',
-  //       link: 'https://github.com/RyanFitzgerald/devfolio',
-  //     },
-  //   ],
-  //   // Optional: List your skills, they must have `name` and `description`.
-  //   skills: [
-  //     {
-  //       name: 'Languages & Frameworks',
-  //       description:
-  //         'JavaScript (ES6+), Golang, Node.js, Express.js, React, Ruby on Rails, PHP',
-  //     },
-  //     {
-  //       name: 'Databases',
-  //       description: 'MongoDB, PostreSQL, MySQL',
-  //     },
-  //     {
-  //       name: 'Other',
-  //       description:
-  //         'Docker, Amazon Web Services (AWS), CI / CD, Microservices, API design, Agile / Scrum',
-  //     },
-  //   ],
-  // },
+    // projects: [
+    //   {
+    //     name: 'Devfolio',
+    //     description:
+    //       'A zero-config and blazing fast personal site + blog built with GatsbyJs and TailwindCSS',
+    //     link: 'https://github.com/RyanFitzgerald/devfolio',
+    //   },
+    //   {
+    //     name: 'ChromeExtensionKit',
+    //     description:
+    //       'Kit to jump-start your Chrome extension projects with a variety of battle-tested starter templates',
+    //     link: 'https://chromeextensionkit.com/?ref=devfolio',
+    //   },
+    //   {
+    //     name: 'Another Cool Project',
+    //     description:
+    //       'Lorem ipsum dolor sit amet consectetur adipisicing elit ducimus perferendis',
+    //     link: 'https://github.com/RyanFitzgerald/devfolio',
+    //   },
+    // ],
+    // // Optional: List your experience, they must have `name` and `description`. `link` is optional.
+    // experience: [
+    //   {
+    //     name: 'Acme Corp',
+    //     description: 'Full-Stack Developer, February 2020 - Present',
+    //     link: 'https://github.com/RyanFitzgerald/devfolio',
+    //   },
+    //   {
+    //     name: 'Globex Corp',
+    //     description: 'Full-Stack Developer, December 2017 - February 2020',
+    //     link: 'https://github.com/RyanFitzgerald/devfolio',
+    //   },
+    //   {
+    //     name: 'Hooli',
+    //     description: 'Full-Stack Developer, May 2015 - December 2017',
+    //     link: 'https://github.com/RyanFitzgerald/devfolio',
+    //   },
+    // ],
+    // // Optional: List your skills, they must have `name` and `description`.
+    // skills: [
+    //   {
+    //     name: 'Languages & Frameworks',
+    //     description:
+    //       'JavaScript (ES6+), Golang, Node.js, Express.js, React, Ruby on Rails, PHP',
+    //   },
+    //   {
+    //     name: 'Databases',
+    //     description: 'MongoDB, PostreSQL, MySQL',
+    //   },
+    //   {
+    //     name: 'Other',
+    //     description:
+    //       'Docker, Amazon Web Services (AWS), CI / CD, Microservices, API design, Agile / Scrum',
+    //   },
+    // ],
+  },
 
   plugins: [
 
